c9f9cdeac2daf276f59600513d3c964ce6a8dc02
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-10-14 15:19:09 -0500

    Create Book class with properties and methods

--- library_management.js
+++ library_management.js
@@ -1,1 +1,26 @@
 // Task 1: Create a Book Class
+
+class Book {
+    constructor(title, author, ISBN, _isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this._isAvailable = _isAvailable;
+    }
+
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.ISBN}`;
+    }
+
+    get isAvailable() {
+        return this._isAvailable;
+    }
+
+    set isAvailable(status) {
+        this._isAvailable = status;
+    }
+}
+
+const myBook = new Book("Star Wars", "George Lucas", 1234, true);
+console.log(myBook.getDetails());
+console.log(myBook.isAvailable);
