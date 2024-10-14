9597b61be3fbf6f3eb20e2c308901f7d1090037d
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-10-14 15:44:35 -0500

    Create Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -24,3 +24,32 @@ class Book {
 const myBook = new Book("Star Wars", "George Lucas", 1234, true);
 console.log(myBook.getDetails());
 console.log(myBook.isAvailable);
+
+// Task 2: Create a Section Class
+
+class Section {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+    
+    addBook(book) {
+        if (book instanceof Book) {
+            this.books.push(book);
+        }
+        else {console.log("Error: Cannot add book objects");}
+    }
+
+    getAvailableBooks() {
+        return this.books.filter(book => book.isAvailable).length;
+    }
+    listBooks() {
+        return this.books.map(book => {return `${book.title} - ${book.isAvailable? 'Available': 'Borrowed'}`;})
+    }
+}
+
+const fictionSection = new Section("Fiction");
+const book1 = new Book("Star Wars Episode I", "Author: George Lucas", "ISBN: 2345");
+fictionSection.addBook(book1);
+console.log(fictionSection.listBooks());
+console.log(fictionSection.getAvailableBooks());
