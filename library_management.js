e398dc5148a198e7f0f1de8446b27ccc3ec67363
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-10-15 12:21:08 -0500

    Create and manage library sections and patrons

--- library_management.js
+++ library_management.js
@@ -66,13 +66,20 @@ class Section {
 
 // Test case
 const fictionSection = new Section("Fiction");
+const nonFictionSection = new Section("Non Fiction");
+const book6 = new Book("The Accidental Superpower", "Author: Peter Zeihan", "ISBN: 7865");
+const book7 = new Book("The Absent Superpower", "Author: Peter Zeihan", "ISBN: 7825");
 const book1 = new Book("Star Wars Episode I", "Author: George Lucas", "ISBN: 2345");
 const book5 = new Book("Star Wars Episode V", "Author: George Lucas", "ISBN: 5567");
 fictionSection.addBook(book1);
 fictionSection.addBook(book5);
+nonFictionSection.addBook(book6);
+nonFictionSection.addBook(book7);
 console.log(fictionSection.listBooks());
 console.log(fictionSection.getAvailableBooks());
 console.log(fictionSection.calculateTotalBooksAvailable());
+console.log(nonFictionSection.calculateTotalBooksAvailable());
+
 
 // Task 3: Create a Patron Class
 
@@ -166,11 +173,17 @@ findCurrentBorrower(book) {
 
 const nonVIP_patron = new Patron("Bob");
 const VIP_patron = new VIPPatron("Sam");
+const VIP_patron1 = new VIPPatron("Luke");
 const book3 = new Book("Star Wars Episode II", "Author: George Lucas", "ISBN: 5678");
 const book4 = new Book("Star Wars Episode III", "Author: George Lucas", "ISBN: 8678");
 
 nonVIP_patron.borrowBook(book3);
 VIP_patron.borrowBook(book4);
+VIP_patron1.borrowBook(book7);
+VIP_patron1.returnBook(book7);
+VIP_patron1.borrowBook(book6);
+console.log(`Non Fiction Books Available: ${nonFictionSection.calculateTotalBooksAvailable()}`);
+console.log(`Fiction Books Available: ${fictionSection.calculateTotalBooksAvailable()}`);
 
 // Task 5: Handle Books Borrowing and Returning
 // Extension of Task 2
@@ -219,6 +232,165 @@ console.log(fictionSection.calculateTotalBooksAvailable());
 
 */
 
+// Task 6:  Create and Manage Sections and Patrons
+// Extension of previous tasks
+
+/*
+
+// Task 2: Create a Section Class
+
+// Defined class section with constructor name as parameter as well as name as property and books as an empty array
+class Section {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+    
+    // addBook method checks if the argument is an instance of the Book class, if not, it logs an error
+    addBook(book) {
+        if (book instanceof Book) {
+            this.books.push(book);
+        }
+        else {console.log("Error: Cannot add book objects");}
+    }
+
+    // Used the filter method to create an array of all available and borrowed books
+    getAvailableBooks() {
+        return this.books.filter(book => book.isAvailable).length;
+    }
+
+    // Used the map method to create an array of strings each representing a book
+    listBooks() {
+        return this.books.map(book => {return `${book.title} - ${book.isAvailable? 'Available': 'Borrowed'}`;})
+    }
+
+    calculateTotalBooksAvailable() {
+        return this.books.reduce((total, book) => {
+            return total + (book.isAvailable ? 1:0);
+        },0);
+    }
+}
+
+// Test case
+const fictionSection = new Section("Fiction");
+const nonFictionSection = new Section("Non Fiction");
+const book6 = new Book("The Accidental Superpower", "Author: Peter Zeihan", "ISBN: 7865");
+const book7 = new Book("The Absent Superpower", "Author: Peter Zeihan", "ISBN: 7825");
+const book1 = new Book("Star Wars Episode I", "Author: George Lucas", "ISBN: 2345");
+const book5 = new Book("Star Wars Episode V", "Author: George Lucas", "ISBN: 5567");
+fictionSection.addBook(book1);
+fictionSection.addBook(book5);
+nonFictionSection.addBook(book6);
+nonFictionSection.addBook(book7);
+console.log(fictionSection.listBooks());
+console.log(fictionSection.getAvailableBooks());
+console.log(fictionSection.calculateTotalBooksAvailable());
+console.log(nonFictionSection.calculateTotalBooksAvailable());
 
 
+// Task 3: Create a Patron Class
+
+// Defined the Patron class with a constructor name as parameter
+class Patron {
+    constructor(name) {
+        this.name = name;
+        this.borrowedBooks = [];
+    }
+    // The borrowBook method checks if the argument book is an instance of the book class
+    borrowBook(book) {
+        if (book instanceof Book) {
+            // If it is a book and isAvailable, set the availability to false and add it to the borrowedBooks Array
+            if (book.isAvailable) {
+                book.isAvailable = false;
+                this.borrowedBooks.push(book);
+                console.log(`${this.name} Has succesfully borrowed ${book.title}.`);
+            }
+            // If the book is not available, it logs a message
+            else {
+                console.log(`${book.title} is not available.`);
+            }
+        }
+        else {
+            console.log("Error: Can only borrow book objects.");
+        }
+    }
+
+    // The returnBook method checks if the book is an instance of Book
+    returnBook(book) {
+        if (book instanceof Book) {
+            // Stored the initial length of the borrowedBooks array
+            const initialLength = this.borrowedBooks.length;
+            // Used filter to create an new array that excludes the book being returned
+            this.borrowedBooks = this.borrowedBooks.filter(b => b.ISBN !== book.ISBN);
+            // Compared the new lengthed of borrowedBooks with initialLength, if it is smaller, it means the book was found and removed
+            if (this.borrowedBooks.length < initialLength) {
+                book.isAvailable = true;
+                console.log(`${this.name} has succesfully returned ${book.title}.`);
+            }
+            else {
+                console.log(`Error: ${book.title} was not borrowed.`);
+            }
+        }
+        else {
+            console.log("Error: Can only return book objects.");
+        }
+    }
+}
+// Test case
+const patron = new Patron("Jack");
+const book2 = new Book("Killer App", "Author: Chunka", "ISBN: 87584");
+patron.borrowBook(book2);
+patron.returnBook(book1);
+
+// Task 4: Create a VIPPatron Class that Inherits from Patron
+
+class VIPPatron extends Patron {
+    constructor(name) {
+        super(name);
+        this.priority = true;
+    }
+    borrowBook(book) {
+        if (book instanceof Book) {
+            // If it is a book and isAvailable, set the availability to false and add it to the borrowedBooks Array
+            if (book.isAvailable) {
+                book.isAvailable = false;
+                this.borrowedBooks.push(book);
+                console.log(`VIP Patron: ${this.name} Has succesfully borrowed ${book.title}`);
+            }
+            else {
+                const currentBorrower = this.findCurrentBorrower(book);
+                if (currentBorrower && !(currentBorrower instanceof VIPPatron)) {
+                    currentBorrower.returnBook(book);
+                    this.borrowedBooks.push(book);
+                    book.isAvailable = false;
+                    console.log(`VIP Patron ${this.name} has succesfully borrowed ${book.title} with priority.`);
+                }
+            
+            // If the book is not available, it logs a message
+            else {
+                console.log(`${book.title} is not available.`);
+            }
+        }
+    }
+}
+findCurrentBorrower(book) {
+    return null;
+}
+}
+
+const nonVIP_patron = new Patron("Bob");
+const VIP_patron = new VIPPatron("Sam");
+const VIP_patron1 = new VIPPatron("Luke");
+const book3 = new Book("Star Wars Episode II", "Author: George Lucas", "ISBN: 5678");
+const book4 = new Book("Star Wars Episode III", "Author: George Lucas", "ISBN: 8678");
+
+nonVIP_patron.borrowBook(book3);
+VIP_patron.borrowBook(book4);
+VIP_patron1.borrowBook(book7);
+VIP_patron1.returnBook(book7);
+VIP_patron1.borrowBook(book6);
+console.log(`Non Fiction Books Available: ${nonFictionSection.calculateTotalBooksAvailable()}`);
+console.log(`Fiction Books Available: ${fictionSection.calculateTotalBooksAvailable()}`);
+
+*/
 
